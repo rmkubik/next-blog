3539efc9d9960761c36f81629420fddbcdd09f0b
Ryan Kubik <rmkubik@me.com>
2020-10-22 22:18:51 -0700

    Copy index make mock blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
+import { MDXProvider } from "@mdx-js/react";
 import "normalize.css";
 import "../styles/main.scss";
+import * as layoutComponents from "../src/layout";
+const { ListItem, Wrapper, Link, ...shortcodes } = layoutComponents;
+
+const components = {
+  li: ListItem,
+  wrapper: Wrapper,
+  a: ({ children, href }) => {
+    return <Link to={href}>{children}</Link>;
+  },
+  // expose following components as shortcodes
+  Link,
+  ...shortcodes,
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MDXProvider components={components}>
+      <Component {...pageProps} />
+    </MDXProvider>
+  );
 }
 
 export default MyApp;
