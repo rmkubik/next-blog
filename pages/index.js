d85c6c7d164d06ecfe3c2d0ba017f9e61e54f0d1
Ryan Kubik <rmkubik@me.com>
2020-11-29 12:38:20 -0800

    Create v1 of project catridges

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,137 @@ const Image = ({ children, src = "", alt, slug, ...rest }) => {
 
   return (
     <>
-      <img
-        alt={alt}
-        src={`/images/projects/${slug}/${relativeStartStripped}`}
-        {...rest}
-      />
+      <div className="square-aspect-ratio">
+        <div className="inner center">
+          <img
+            alt={alt}
+            src={`/images/projects/${slug}/${relativeStartStripped}`}
+            {...rest}
+          />
+          <div className="overlay" />
+        </div>
+      </div>
       <style jsx>{`
         img {
-          max-width: 100%;
-          margin-left: auto;
-          margin-right: auto;
-          display: block;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+
+        .overlay {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          box-shadow: inset 0 0 16px #222;
+        }
+
+        .center {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+
+        .square-aspect-ratio {
+          position: relative;
+
+          &:before {
+            display: block;
+            content: "";
+            width: 100%;
+            padding-top: (1 / 1) * 100%;
+          }
+
+          > .inner {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+          }
+        }
+      `}</style>
+    </>
+  );
+};
+
+const Project = ({ project }) => {
+  const imageSrc =
+    typeof project.frontmatter.thumbnail === "object"
+      ? project.frontmatter.thumbnail.src
+      : project.frontmatter.thumbnail;
+  const bgColor = project.frontmatter.thumbnail.bgColor || "#d3e2eb";
+  const titleColor = project.frontmatter.thumbnail.titleColor || "inherit";
+
+  return (
+    <>
+      <li>
+        <div className="logo">{project.frontmatter.title}</div>
+        <div className="label">
+          <Image
+            alt={project.frontmatter.title}
+            slug={project.slug}
+            src={imageSrc}
+          />
+        </div>
+      </li>
+      <style jsx>{`
+        li {
+          padding: 1rem;
+          background-color: #d3e2eb;
+          border-radius: 6px;
+          border-top-right-radius: 22px;
+
+          border: 3px outset #738e9e;
+
+          box-shadow: 2px 2px 8px #222;
+
+          &:hover {
+            transition: ease-out 200ms;
+            transition-property: all;
+            transform: scale(1.1);
+            cursor: pointer;
+            box-shadow: 2px 2px 16px #222;
+          }
+        }
+
+        .logo {
+          height: 1.8rem;
+          box-shadow: inset 0 0 16px #222;
+          margin-bottom: 1rem;
+          border-radius: 12px;
+
+          border: 3px inset #738e9e;
+
+          background-color: white;
+          text-align: center;
+          color: #222;
+          line-height: 1.8rem;
+        }
+
+        .label {
+          margin-bottom: 1rem;
+          margin: 0.3rem;
+
+          border: 3px inset #738e9e;
+
+          h3 {
+            font-size: 1.3rem;
+            margin: 0;
+            margin-bottom: 1rem;
+          }
+        }
+      `}</style>
+      <style jsx>{`
+        .label {
+          background-color: ${bgColor};
+
+          h3 {
+            color: ${titleColor};
+          }
         }
       `}</style>
     </>
@@ -34,14 +154,7 @@ const Home = ({ projects }) => {
         <h2>{"Some of my projects"}</h2>
         <ul>
           {projects.map((project, index) => (
-            <li key={index}>
-              <h3>{project.frontmatter.title}</h3>
-              <Image
-                alt=""
-                slug={project.slug}
-                src={project.frontmatter.thumbnail}
-              />
-            </li>
+            <Project key={index} project={project} />
           ))}
         </ul>
       </Section>
@@ -71,19 +184,6 @@ const Home = ({ projects }) => {
 
           padding: 0;
           list-style: none;
-
-          li {
-            border: 2px solid black;
-            border-radius: 4px;
-            padding: 1rem;
-            background-color: #d3e2eb;
-          }
-
-          h3 {
-            font-size: 1.3rem;
-            margin: 0;
-            margin-bottom: 1rem;
-          }
         }
       `}</style>
     </div>
