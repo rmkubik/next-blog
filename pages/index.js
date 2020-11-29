4fc1bbe7f0d75051db81c42ae3bc2150a6703fd7
Ryan Kubik <rmkubik@me.com>
2020-11-29 14:17:00 -0800

    WIP: featured blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,150 +1,6 @@
 import Section from "../src/components/Section";
 import { getAllPosts } from "../src/services/posts";
-
-const Image = ({ children, src = "", alt, slug, ...rest }) => {
-  const relativeStartStripped = src.replace(/^.\//u, "");
-
-  return (
-    <>
-      <div className="square-aspect-ratio">
-        <div className="inner center">
-          <img
-            alt={alt}
-            src={`/images/projects/${slug}/${relativeStartStripped}`}
-            {...rest}
-          />
-          <div className="overlay" />
-        </div>
-      </div>
-      <style jsx>{`
-        img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-        }
-
-        .overlay {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          box-shadow: inset 0 0 16px #222;
-        }
-
-        .center {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-
-        .square-aspect-ratio {
-          position: relative;
-
-          &:before {
-            display: block;
-            content: "";
-            width: 100%;
-            padding-top: (1 / 1) * 100%;
-          }
-
-          > .inner {
-            position: absolute;
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-          }
-        }
-      `}</style>
-    </>
-  );
-};
-
-const Project = ({ project }) => {
-  const imageSrc =
-    typeof project.frontmatter.thumbnail === "object"
-      ? project.frontmatter.thumbnail.src
-      : project.frontmatter.thumbnail;
-  const bgColor = project.frontmatter.thumbnail.bgColor || "#d3e2eb";
-  const titleColor = project.frontmatter.thumbnail.titleColor || "inherit";
-
-  return (
-    <>
-      <li>
-        <div className="logo">{project.frontmatter.title}</div>
-        <div className="label">
-          <Image
-            alt={project.frontmatter.title}
-            slug={project.slug}
-            src={imageSrc}
-          />
-        </div>
-      </li>
-      <style jsx>{`
-        li {
-          padding: 1rem;
-          background-color: #d3e2eb;
-          border-radius: 6px;
-          border-top-right-radius: 22px;
-
-          border: 3px outset #738e9e;
-          background-image: url("/images/french-stucco.png");
-
-          box-shadow: 2px 2px 8px #222;
-
-          &:hover {
-            transition: ease-out 200ms;
-            transition-property: all;
-            transform: scale(1.1);
-            cursor: pointer;
-            box-shadow: 2px 2px 16px #222;
-          }
-        }
-
-        .logo {
-          height: 1.8rem;
-          box-shadow: inset 0 0 14px #222;
-          margin-bottom: 1rem;
-          border-radius: 12px;
-
-          border: 3px inset #738e9e;
-
-          background-color: #eee;
-          background-image: url("/images/white-brushed.png");
-
-          text-align: center;
-          color: #222;
-          line-height: 1.8rem;
-        }
-
-        .label {
-          margin-bottom: 1rem;
-          margin: 0.3rem;
-
-          border: 3px inset #738e9e;
-
-          h3 {
-            font-size: 1.3rem;
-            margin: 0;
-            margin-bottom: 1rem;
-          }
-        }
-      `}</style>
-      <style jsx>{`
-        .label {
-          background-color: ${bgColor};
-
-          h3 {
-            color: ${titleColor};
-          }
-        }
-      `}</style>
-    </>
-  );
-};
+import Cartridge from "../src/components/projects/Cartridge";
 
 const Home = ({ projects }) => {
   return (
@@ -154,22 +10,65 @@ const Home = ({ projects }) => {
         <p>{"I make video games, usually with JavaScript."}</p>
       </Section>
       <Section>
-        <h2>{"Some of my projects"}</h2>
-        <ul>
-          {projects.map((project) => (
-            <Project key={project.slug} project={project} />
-          ))}
-        </ul>
+        <h2>{"I make games"}</h2>
       </Section>
+      <ul>
+        {projects.map((project) => (
+          <Cartridge key={project.slug} project={project} />
+        ))}
+      </ul>
+      <Section>
+        <h2>{"I write a bit too"}</h2>
+      </Section>
+      <ul className="writing">
+        <li>
+          <h3>
+            {
+              "Wildfire Swap: A puzzle game inspired by fires in the Pacific Northwest"
+            }
+          </h3>
+          <p>
+            {
+              "Wildfire Swap is a puzzle game about swapping tiles to prevent fires from burning down people's homes. While showing Wildfire Swap to folks, I've gotten recurring questions about what inspired this game. This post dives into that inspiration story and looks at where Wildfire Swap came from. The I..."
+            }
+          </p>
+        </li>
+        <li>
+          <h3>
+            {
+              "Wildfire Swap: A puzzle game inspired by fires in the Pacific Northwest"
+            }
+          </h3>
+          <p>
+            {
+              "Wildfire Swap is a puzzle game about swapping tiles to prevent fires from burning down people's homes. While showing Wildfire Swap to folks, I've gotten recurring questions about what inspired this game. This post dives into that inspiration story and looks at where Wildfire Swap came from. The I..."
+            }
+          </p>
+        </li>
+        <li>
+          <h3>
+            {
+              "Wildfire Swap: A puzzle game inspired by fires in the Pacific Northwest"
+            }
+          </h3>
+          <p>
+            {
+              "Wildfire Swap is a puzzle game about swapping tiles to prevent fires from burning down people's homes. While showing Wildfire Swap to folks, I've gotten recurring questions about what inspired this game. This post dives into that inspiration story and looks at where Wildfire Swap came from. The I..."
+            }
+          </p>
+        </li>
+      </ul>
       <style jsx>{`
         .main {
           h1 {
             margin-bottom: 2rem;
           }
 
-          & > :global(*:first-child) {
+          :global(> *) {
             margin-bottom: 2rem;
+          }
 
+          & > :global(*:first-child) {
             h1 {
               margin-bottom: 1rem;
             }
@@ -187,6 +86,28 @@ const Home = ({ projects }) => {
 
           padding: 0;
           list-style: none;
+        }
+
+        .writing {
+          li {
+            background-image: url("/images/groovepaper.png");
+            padding: 1rem;
+            box-shadow: 4px 4px 16px black;
+            background-color: #d3e2eb;
+            border: 2px solid black;
+
+            h3 {
+              margin-top: 0;
+            }
+
+            &:hover {
+              transition: ease-out 200ms;
+              transition-property: all;
+              transform: scale(1.1);
+              cursor: pointer;
+              box-shadow: 2px 2px 16px #222;
+            }
+          }
         }
       `}</style>
     </div>
---
+++ src/components/projects/Cartridge.js
@@ -0,0 +1,146 @@
+const Image = ({ children, src = "", alt, slug, ...rest }) => {
+  const relativeStartStripped = src.replace(/^.\//u, "");
+
+  return (
+    <>
+      <div className="square-aspect-ratio">
+        <div className="inner center">
+          <img
+            alt={alt}
+            src={`/images/projects/${slug}/${relativeStartStripped}`}
+            {...rest}
+          />
+          <div className="overlay" />
+        </div>
+      </div>
+      <style jsx>{`
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+
+        .overlay {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          box-shadow: inset 0 0 16px #222;
+        }
+
+        .center {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+
+        .square-aspect-ratio {
+          position: relative;
+
+          &:before {
+            display: block;
+            content: "";
+            width: 100%;
+            padding-top: (1 / 1) * 100%;
+          }
+
+          > .inner {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+          }
+        }
+      `}</style>
+    </>
+  );
+};
+
+const Cartridge = ({ project }) => {
+  const imageSrc =
+    typeof project.frontmatter.thumbnail === "object"
+      ? project.frontmatter.thumbnail.src
+      : project.frontmatter.thumbnail;
+  const bgColor = project.frontmatter.thumbnail.bgColor || "#d3e2eb";
+  const titleColor = project.frontmatter.thumbnail.titleColor || "inherit";
+
+  return (
+    <>
+      <li>
+        <div className="logo">{project.frontmatter.title}</div>
+        <div className="label">
+          <Image
+            alt={project.frontmatter.title}
+            slug={project.slug}
+            src={imageSrc}
+          />
+        </div>
+      </li>
+      <style jsx>{`
+        li {
+          padding: 1rem;
+          background-color: #d3e2eb;
+          border-radius: 6px;
+          border-top-right-radius: 22px;
+
+          border: 3px outset #738e9e;
+          background-image: url("/images/french-stucco.png");
+
+          box-shadow: 2px 2px 8px #222;
+
+          &:hover {
+            transition: ease-out 200ms;
+            transition-property: all;
+            transform: scale(1.1);
+            cursor: pointer;
+            box-shadow: 2px 2px 16px #222;
+          }
+        }
+
+        .logo {
+          height: 1.8rem;
+          box-shadow: inset 0 0 14px #222;
+          margin-bottom: 1rem;
+          border-radius: 12px;
+
+          border: 3px inset #738e9e;
+
+          background-color: #eee;
+          background-image: url("/images/white-brushed.png");
+
+          text-align: center;
+          color: #222;
+          line-height: 1.8rem;
+        }
+
+        .label {
+          margin-bottom: 1rem;
+          margin: 0.3rem;
+
+          border: 3px inset #738e9e;
+
+          h3 {
+            font-size: 1.3rem;
+            margin: 0;
+            margin-bottom: 1rem;
+          }
+        }
+      `}</style>
+      <style jsx>{`
+        .label {
+          background-color: ${bgColor};
+
+          h3 {
+            color: ${titleColor};
+          }
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default Cartridge;
