dcc667cc429f5921bb1663fdc41c1dd7ea30880b
Ryan Kubik <rmkubik@me.com>
2020-11-30 23:50:28 -0800

    Add posts and projects to index

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,122 @@
+import Link from "../src/components/Link";
 import Section from "../src/components/Section";
-import { getAllPosts } from "../src/services/posts";
-import Cartridge from "../src/components/projects/Cartridge";
+import { getAllPosts, getMdxSourceBySlug } from "../src/services/posts";
 
-const Home = ({ projects }) => {
+const Image = ({ children, src = "", alt, slug, ...rest }) => {
+  const relativeStartStripped = src.replace(/^.\//u, "");
+
+  return (
+    <>
+      <div className="square-aspect-ratio">
+        <div className="inner center">
+          <img
+            alt={alt}
+            src={`/images/projects/${slug}/${relativeStartStripped}`}
+            {...rest}
+          />
+          <div className="overlay" />
+        </div>
+      </div>
+      <style jsx>{`
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+
+        .overlay {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          box-shadow: inset 0 0 16px #222;
+        }
+
+        .center {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+
+        .square-aspect-ratio {
+          position: relative;
+
+          &:before {
+            display: block;
+            content: "";
+            width: 100%;
+            padding-top: (1 / 1) * 100%;
+          }
+
+          > .inner {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+          }
+        }
+      `}</style>
+    </>
+  );
+};
+
+const Home = ({ posts, projects }) => {
   return (
     <div className="main">
       <Section>
-        <h1>{"Hi!"}</h1>
-        <p>{"I make video games, usually with JavaScript."}</p>
+        <h1>{"Hi, I make games"}</h1>
+        <p>{"Usually with JavaScript."}</p>
+        <p>
+          {
+            "Below are some of the better projects I've made by myself or in teams. A lot of them were made for game jams. A few of them are longer term personal or work projects."
+          }
+        </p>
       </Section>
-      <Section>
-        <h2>{"I make games"}</h2>
-      </Section>
-      <ul>
-        {projects.map((project) => (
-          <Cartridge key={project.slug} project={project} />
-        ))}
-      </ul>
+      <div className="projects">
+        {projects.map((project) => {
+          const imageSrc =
+            typeof project.frontmatter.thumbnail === "object"
+              ? project.frontmatter.thumbnail.src
+              : project.frontmatter.thumbnail;
+
+          return (
+            <Section key={project.frontmatter.title}>
+              <h3>{project.frontmatter.title}</h3>
+              <Image
+                alt={project.frontmatter.title}
+                slug={project.slug}
+                src={imageSrc}
+              />
+            </Section>
+          );
+        })}
+      </div>
       <Section>
         <h2>{"I write a bit too"}</h2>
+        <p>
+          {"Most of my writing is "}
+          <Link hideDots to="/blog">
+            {"posted on my blog"}
+          </Link>
+          {
+            ". A lot of my posts are more informational pieces on specific topics or solutions to problems I've run into. Below are some of the more thought out and interesting posts."
+          }
+        </p>
       </Section>
-      <ul className="writing">
-        <li>
-          <h3>
-            {
-              "Wildfire Swap: A puzzle game inspired by fires in the Pacific Northwest"
-            }
-          </h3>
-          <p>
-            {
-              "Wildfire Swap is a puzzle game about swapping tiles to prevent fires from burning down people's homes. While showing Wildfire Swap to folks, I've gotten recurring questions about what inspired this game. This post dives into that inspiration story and looks at where Wildfire Swap came from. The I..."
-            }
-          </p>
-        </li>
-        <li>
-          <h3>
-            {
-              "Wildfire Swap: A puzzle game inspired by fires in the Pacific Northwest"
-            }
-          </h3>
-          <p>
-            {
-              "Wildfire Swap is a puzzle game about swapping tiles to prevent fires from burning down people's homes. While showing Wildfire Swap to folks, I've gotten recurring questions about what inspired this game. This post dives into that inspiration story and looks at where Wildfire Swap came from. The I..."
-            }
-          </p>
-        </li>
-        <li>
-          <h3>
-            {
-              "Wildfire Swap: A puzzle game inspired by fires in the Pacific Northwest"
-            }
-          </h3>
-          <p>
-            {
-              "Wildfire Swap is a puzzle game about swapping tiles to prevent fires from burning down people's homes. While showing Wildfire Swap to folks, I've gotten recurring questions about what inspired this game. This post dives into that inspiration story and looks at where Wildfire Swap came from. The I..."
-            }
-          </p>
-        </li>
-      </ul>
+      <div className="writing">
+        {posts.map((post) => {
+          return (
+            <Section key={post.frontmatter.title}>
+              <h3>{post.frontmatter.title}</h3>
+              <Link to={`/blog/${post.slug}`}>{post.readingTime}</Link>
+            </Section>
+          );
+        })}
+      </div>
       <style jsx>{`
         .main {
           h1 {
@@ -74,38 +133,34 @@ const Home = ({ projects }) => {
             }
 
             p {
-              margin: 0;
+              margin-bottom: 0;
             }
           }
         }
 
-        ul {
+        .projects {
           display: grid;
           grid-template-columns: 1fr 1fr 1fr;
           grid-gap: 1rem;
 
-          padding: 0;
-          list-style: none;
+          :global(section) {
+            padding: 1rem;
+            text-align: center;
+          }
         }
 
         .writing {
-          li {
-            background-image: url("/images/groovepaper.png");
-            padding: 1rem;
-            box-shadow: 4px 4px 16px black;
-            background-color: #d3e2eb;
-            border: 2px solid black;
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 1rem;
 
-            h3 {
-              margin-top: 0;
-            }
+          :global(section) {
+            display: flex;
+            flex-direction: column;
+            padding: 2rem;
 
-            &:hover {
-              transition: ease-out 200ms;
-              transition-property: all;
-              transform: scale(1.1);
-              cursor: pointer;
-              box-shadow: 2px 2px 16px #222;
+            :global(a) {
+              margin-top: auto;
             }
           }
         }
@@ -116,9 +171,31 @@ const Home = ({ projects }) => {
 
 export const getStaticProps = async () => {
   const projects = await getAllPosts("projects");
+  const featuredPostSlugs = [
+    "wildfire-swap-inspiration",
+    "deliberate-game-jamming",
+    "wildfire-swap-design-pillars",
+    "eslint-internal-state",
+    "showcases-season-retrospective",
+  ];
+  const postPromises = featuredPostSlugs.map(async (slug) => {
+    const { frontmatter, readingTime } = await getMdxSourceBySlug(
+      "posts",
+      slug,
+      {}
+    );
+
+    return {
+      frontmatter,
+      readingTime,
+      slug,
+    };
+  });
+  const posts = await Promise.all(postPromises);
 
   return {
     props: {
+      posts,
       projects,
     },
   };
