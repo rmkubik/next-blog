00034dc38677a4931403681368511ba05a470191
Ryan Kubik <rmkubik@me.com>
2023-05-16 12:18:52 -0700

    Add corner bounds, remove static obastcles, set up collision masks

--- pages/work.js
+++ pages/work.js
@@ -121,6 +121,54 @@ const Projects = () => {
 
             const containerBoundingRect = container.getBoundingClientRect();
 
+            const boundsThickness = 100;
+
+            const bounds = [
+              // top bound
+              Bodies.rectangle(
+                containerBoundingRect.width / 2,
+                -boundsThickness / 2,
+                containerBoundingRect.width + boundsThickness * 2,
+                boundsThickness,
+                { isStatic: true }
+              ),
+              // bottom bound
+              Bodies.rectangle(
+                containerBoundingRect.width / 2,
+                containerBoundingRect.height + boundsThickness / 2,
+                containerBoundingRect.width + boundsThickness * 2,
+                boundsThickness,
+                { isStatic: true }
+              ),
+              // right bound
+              Bodies.rectangle(
+                containerBoundingRect.width + boundsThickness / 2,
+                containerBoundingRect.height / 2,
+                boundsThickness,
+                containerBoundingRect.height + boundsThickness * 2,
+                { isStatic: true }
+              ),
+              // left bound
+              Bodies.rectangle(
+                -boundsThickness / 2,
+                containerBoundingRect.height / 2,
+                boundsThickness,
+                containerBoundingRect.height + boundsThickness * 2,
+                { isStatic: true }
+              ),
+            ];
+
+            bounds.forEach((bound) => {
+              // eslint-disable-next-line no-param-reassign
+              bound.restitution = 0.8;
+              // eslint-disable-next-line no-param-reassign
+              bound.collisionFilter.category = 0x1_00;
+              // eslint-disable-next-line no-param-reassign
+              bound.collisionFilter.mask = 0x0_01;
+            });
+
+            Composite.add(engine.world, bounds);
+
             for (const obstacleElement of obstacleElements) {
               const obstacleBoundingRect =
                 obstacleElement.getBoundingClientRect();
@@ -133,7 +181,12 @@ const Projects = () => {
                   obstacleBoundingRect.height / 2,
                 obstacleBoundingRect.width,
                 obstacleBoundingRect.height
+                // { isStatic: true }
               );
+
+              body.restitution = 0.8;
+              body.collisionFilter.category = 0x0_10;
+              body.collisionFilter.mask = 0x0_11;
 
               // eslint-disable-next-line no-undef
               const graphics = new PIXI.Graphics();
@@ -168,6 +221,10 @@ const Projects = () => {
               graphics,
               radius,
             };
+
+            Body.setInertia(ball.body, Number.POSITIVE_INFINITY);
+            ball.body.collisionFilter.category = 0x0_01;
+            ball.body.collisionFilter.mask = 0x1_11;
 
             ball.graphics.beginFill("#FF6961");
             ball.graphics.drawCircle(0, 0, ball.radius);
